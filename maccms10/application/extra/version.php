0ce8644180046303dfa578c91c27757b41a800aa
52xd <admin@52xd.tv>
2020-12-10 19:38:02 +0800

    更新最新版海螺主题
    更新最新版海螺主题

---
+++ maccms10/application/extra/version.php
@@ -0,0 +1,9 @@
+<?php
+return array (
+    'name' => '苹果CMS',
+    'copyright' => 'MacCMS',
+    'url' => '//github.com/magicblack',
+    'code' => '2020.1000.1052',
+    'license' => '免费版',
+);
+?>
